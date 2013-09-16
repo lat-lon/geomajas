2a4588ca1016652ba01cce396b35c0ecfeb5e870
aerben <erben@lat-lon.de>
2013-09-16 14:45:42 +0200

    Refactored raster layer aggregation in printing component and mapmodel

--- face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/map/MapModel.java
+++ face/geomajas-face-gwt/client/src/main/java/org/geomajas/gwt/client/map/MapModel.java
@@ -364,27 +364,42 @@ public class MapModel implements Paintable, MapViewChangedHandler, HasFeatureSel
 		String currentAggregationId = null;
 		for (Layer<?> layer : layers) {
 			if (layer.isShowing()) {
-				if (layer.getLayerInfo() instanceof ClientLayerInfo) {
-					ClientLayerInfo layerInfo = (ClientLayerInfo) layer.getLayerInfo();
-					String aggregationId = layerInfo.getAggregationId();
-					if (aggregationId == null) {
-						endStreak(aggregation, unvisitedLayers);
-						aggregation.add(layer);
-						currentAggregationId = null;
-					} else {
-						if (!aggregationId.equals(currentAggregationId) && currentAggregationId != null) {
-							endStreak(aggregation, unvisitedLayers);
-							currentAggregationId = aggregationId;
-						}
-						unvisitedLayers.add(layer);
-					}
+				if (layer.getLayerInfo() instanceof ClientRasterLayerInfo) {
+					currentAggregationId = processRasterLayerAggregation(unvisitedLayers, aggregation,
+							currentAggregationId, layer);
+				} else {
+					currentAggregationId = handleNonAggregatableLayer(unvisitedLayers, aggregation, layer);
 				}
 			}
 		}
-		endStreak(aggregation, unvisitedLayers);
+		endAggregationStreak(aggregation, unvisitedLayers);
 		return aggregation;
 	}
 
+	private String processRasterLayerAggregation(List<Layer<?>> unvisitedLayers, List<Layer<?>> aggregation,
+			String currentAggregationId, Layer<?> layer) {
+		ClientLayerInfo layerInfo = (ClientLayerInfo) layer.getLayerInfo();
+		String aggregationId = layerInfo.getAggregationId();
+		if (aggregationId == null) {
+			currentAggregationId = handleNonAggregatableLayer(unvisitedLayers, aggregation, layer);
+		} else {
+			if (!aggregationId.equals(currentAggregationId) && currentAggregationId != null) {
+				endAggregationStreak(aggregation, unvisitedLayers);
+				currentAggregationId = aggregationId;
+			}
+			unvisitedLayers.add(layer);
+		}
+		return currentAggregationId;
+	}
+
+	private String handleNonAggregatableLayer(List<Layer<?>> unvisitedLayers, List<Layer<?>> aggregation, Layer<?> layer) {
+		String currentAggregationId;
+		endAggregationStreak(aggregation, unvisitedLayers);
+		aggregation.add(layer);
+		currentAggregationId = null;
+		return currentAggregationId;
+	}
+	
 	private void clearActiveComboRasterLayers(PainterVisitor visitor, Object group) {
 		for (Layer<?> layer : activeComboRasterLayers) {
 			visitor.remove(layer, group);
@@ -395,27 +410,15 @@ public class MapModel implements Paintable, MapViewChangedHandler, HasFeatureSel
 		activeComboRasterLayers.clear();
 	}
 
-	private void endStreak(List<Layer<?>> aggregation, List<Layer<?>> layers) {
-		if (layers == null || layers.isEmpty())
+	private void endAggregationStreak(List<Layer<?>> aggregation, List<Layer<?>> layers) {
+		if (layers == null || layers.isEmpty()) {
 			return;
-		if (layers.size() == 1) {
+		} else if (layers.size() == 1) {
 			aggregation.add(layers.get(0));
 		} else {
 			aggregation.add(new ComboRasterLayer(layers));
 		}
 	}
-
-	// private L endStreak(PainterVisitor visitor, Object group, Bbox bounds, boolean recursive,
-	// List<Layer<?>> unvisitedLayers) {
-	// if (unvisitedLayers.size() == 1) {
-	// unvisitedLayers.get(0).accept(visitor, group, bounds, recursive);
-	// } else if (unvisitedLayers.size() > 1) {
-	// ComboRasterLayer comboLayer = new ComboRasterLayer(unvisitedLayers);
-	// comboLayer.accept(visitor, group, bounds, recursive);
-	// activeComboRasterLayers.add(comboLayer);
-	// }
-	// unvisitedLayers.clear();
-	// }
 
 	/**
 	 * Return this map model's id.
@@ -692,22 +695,22 @@ public class MapModel implements Paintable, MapViewChangedHandler, HasFeatureSel
 	}
 
 	/**
-     * Search a raster layer by it's id.
-     * 
-     * @param layerId
-     *            The layer's client ID.
-     * @return Returns either a Layer, or null.
-     */
-    public RasterLayer getRasterLayer(String layerId) {
-        if (layers != null) {
-            for (RasterLayer layer : getRasterLayers()) {
-                if (layer.getId().equals(layerId)) {
-                    return layer;
-                }
-            }
-        }
-        return null;
-    }
+	 * Search a raster layer by it's id.
+	 * 
+	 * @param layerId
+	 *            The layer's client ID.
+	 * @return Returns either a Layer, or null.
+	 */
+	public RasterLayer getRasterLayer(String layerId) {
+		if (layers != null) {
+			for (RasterLayer layer : getRasterLayers()) {
+				if (layer.getId().equals(layerId)) {
+					return layer;
+				}
+			}
+		}
+		return null;
+	}
 
 	/**
 	 * Select a new layer. Only one layer can be selected at a time, so this function first tries to deselect the
@@ -745,19 +748,19 @@ public class MapModel implements Paintable, MapViewChangedHandler, HasFeatureSel
 	}
 
 	/**
-     * Return a list containing all raster layers within this model.
-     * 
-     * @return raster layers
-     */
-    public List<RasterLayer> getRasterLayers() {
-        ArrayList<RasterLayer> list = new ArrayList<RasterLayer>();
-        for (Layer<?> layer : layers) {
-            if (layer instanceof RasterLayer) {
-                list.add((RasterLayer) layer);
-            }
-        }
-        return list;
-    }
+	 * Return a list containing all raster layers within this model.
+	 * 
+	 * @return raster layers
+	 */
+	public List<RasterLayer> getRasterLayers() {
+		ArrayList<RasterLayer> list = new ArrayList<RasterLayer>();
+		for (Layer<?> layer : layers) {
+			if (layer instanceof RasterLayer) {
+				list.add((RasterLayer) layer);
+			}
+		}
+		return list;
+	}
 
 	/** Clear the list of selected features in all vector layers. */
 	public void clearSelectedFeatures() {
