6c426d563d9aea0e7d85028585a9b2764a2b6094
Dirk Stenger <stenger@lat-lon.de>
2013-10-18 18:26:13 +0200

    Implemented resize mechanism for legends and improved placement of legend pictures

--- plugin/geomajas-plugin-printing/printing/src/main/java/org/geomajas/plugin/printing/component/impl/DynamicLegendComponentImpl.java
+++ plugin/geomajas-plugin-printing/printing/src/main/java/org/geomajas/plugin/printing/component/impl/DynamicLegendComponentImpl.java
@@ -1,6 +1,14 @@
 package org.geomajas.plugin.printing.component.impl;
 
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.geomajas.plugin.printing.component.PdfContext;
 import org.geomajas.plugin.printing.component.PrintComponent;
@@ -54,23 +62,27 @@ public class DynamicLegendComponentImpl extends AbstractLegendComponentImpl<Dyna
 		float currentHeight = startHeight;
 		float maxWidth = 0;
 
-		for (PrintComponent<?> child : getChildren()) {
+		List<Entry<PrintComponent<?>, Float>> sortedListOfChildren = retrieveSortedListOfChildren();
+		for (Entry<PrintComponent<?>, Float> childEntry : sortedListOfChildren) {
+			PrintComponent<?> child = childEntry.getKey();
 			if (child != titleLabel) {
 				child.layout(context);
 				Rectangle childBounds = child.getBounds();
 				float childWidth = childBounds.getWidth();
-				float childHeight = child.getBounds().getHeight();
+				float childHeight = childBounds.getHeight();
 
 				maxWidth = Math.max(maxWidth, childWidth);
 
 				currentHeight -= childHeight;
 				if (currentHeight < MARGIN) {
-					currentWidth += maxWidth;
+					if ((currentHeight + childHeight) != startHeight)
+						currentWidth += maxWidth;
 					currentHeight = startHeight;
 					maxWidth = childWidth;
+					float resizeInPercentage = currentHeight / childHeight;
 					float llx = currentWidth;
-					float urx = currentWidth + childWidth;
-					float lly = currentHeight - childHeight;
+					float urx = currentWidth + (childWidth * resizeInPercentage);
+					float lly = MARGIN;
 					float ury = currentHeight;
 					child.setBounds(new Rectangle(llx, lly, urx, ury));
 					if (currentHeight - childHeight < MARGIN) {
@@ -85,6 +97,35 @@ public class DynamicLegendComponentImpl extends AbstractLegendComponentImpl<Dyna
 				}
 			}
 		}
+	}
+
+	private List<Entry<PrintComponent<?>, Float>> retrieveSortedListOfChildren() {
+		Map<PrintComponent<?>, Float> map = retrieveMapWithChildren();
+		return retrieveSortedList(map);
+	}
+
+	private Map<PrintComponent<?>, Float> retrieveMapWithChildren() {
+		Map<PrintComponent<?>, Float> map = new HashMap<PrintComponent<?>, Float>();
+		for (PrintComponent<?> child : getChildren()) {
+			if (child != titleLabel) {
+				Rectangle childBounds = child.getBounds();
+				float childHeight = childBounds.getHeight();
+				map.put(child, childHeight);
+			}
+		}
+		return map;
+	}
+
+	private List<Entry<PrintComponent<?>, Float>> retrieveSortedList(Map<PrintComponent<?>, Float> map) {
+		Set<Entry<PrintComponent<?>, Float>> entrySet = map.entrySet();
+		List<Entry<PrintComponent<?>, Float>> arrayList = new ArrayList<Entry<PrintComponent<?>, Float>>(entrySet);
+		Collections.sort(arrayList, new Comparator<Map.Entry<PrintComponent<?>, Float>>() {
+
+			public int compare(Map.Entry<PrintComponent<?>, Float> entry1, Map.Entry<PrintComponent<?>, Float> entry2) {
+				return (entry2.getValue()).compareTo(entry1.getValue());
+			}
+		});
+		return arrayList;
 	}
 
 }
