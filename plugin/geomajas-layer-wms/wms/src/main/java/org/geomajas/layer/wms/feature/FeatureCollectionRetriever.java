ab15cf9e5d9d9278b10e7ca1225e215c3636e849
Lyn Goltz <goltz@lat-lon.de>
2014-09-10 12:50:40 +0200

    changed visibilty; added log statement

--- plugin/geomajas-layer-wms/wms/src/main/java/org/geomajas/layer/wms/feature/FeatureCollectionRetriever.java
+++ plugin/geomajas-layer-wms/wms/src/main/java/org/geomajas/layer/wms/feature/FeatureCollectionRetriever.java
@@ -8,6 +8,8 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.vividsolutions.jts.geom.Envelope;
 
@@ -22,33 +24,36 @@ import com.vividsolutions.jts.geom.Envelope;
  */
 public class FeatureCollectionRetriever {
 
-	/**
-	 * @param baseWfsUrl
-	 *            the url of the WFS including VERSIOn, REQUEST, SERVICE, TYPENAME and RESULTTYPE parameter, RESULTTYPE
-	 *            should be hits, never <code>null</code>
-	 * @param bbox
-	 *            never <code>null</code>
-	 * @return the response of the GetFeature request, never <code>null</code>. The {@link InputStream} must be closed
-	 *         fron the invoking method.
-	 * @throws IOException
-	 * @throws ClientProtocolException
-	 */
-	InputStream retrieveGetFeatureResponseAsInputStream(String baseWfsUrl, Envelope bbox) throws IOException,
-			ClientProtocolException {
-		String requestUrlFromEnvelope = buildRequestUrlFromEnvelope(baseWfsUrl, bbox);
-		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet(requestUrlFromEnvelope);
-		HttpResponse response = client.execute(request);
-		return response.getEntity().getContent();
-	}
+    private final Logger log = LoggerFactory.getLogger( FeatureCollectionRetriever.class );
 
-	private String buildRequestUrlFromEnvelope(String baseWfsUrl, Envelope bbox) {
-		StringBuilder requestUrlBuilder = new StringBuilder(baseWfsUrl);
-		requestUrlBuilder.append("&bbox=");
-		requestUrlBuilder.append(bbox.getMinX()).append(",").append(bbox.getMinY()).append(",");
-		requestUrlBuilder.append(bbox.getMaxX()).append(",").append(bbox.getMaxY()).append(",");
-		requestUrlBuilder.append("EPSG:25833");
-		return requestUrlBuilder.toString();
-	}
+    /**
+     * @param baseWfsUrl
+     *            the url of the WFS including VERSIOn, REQUEST, SERVICE, TYPENAME and RESULTTYPE parameter, RESULTTYPE
+     *            should be hits, never <code>null</code>
+     * @param bbox
+     *            never <code>null</code>
+     * @return the response of the GetFeature request, never <code>null</code>. The {@link InputStream} must be closed
+     *         fron the invoking method.
+     * @throws IOException
+     * @throws ClientProtocolException
+     */
+    public InputStream retrieveGetFeatureResponseAsInputStream( String baseWfsUrl, Envelope bbox )
+                            throws IOException, ClientProtocolException {
+        String requestUrlFromEnvelope = buildRequestUrlFromEnvelope( baseWfsUrl, bbox );
+        log.debug( "GetFeature request: " + requestUrlFromEnvelope );
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet( requestUrlFromEnvelope );
+        HttpResponse response = client.execute( request );
+        return response.getEntity().getContent();
+    }
+
+    private String buildRequestUrlFromEnvelope( String baseWfsUrl, Envelope bbox ) {
+        StringBuilder requestUrlBuilder = new StringBuilder( baseWfsUrl );
+        requestUrlBuilder.append( "&bbox=" );
+        requestUrlBuilder.append( bbox.getMinX() ).append( "," ).append( bbox.getMinY() ).append( "," );
+        requestUrlBuilder.append( bbox.getMaxX() ).append( "," ).append( bbox.getMaxY() ).append( "," );
+        requestUrlBuilder.append( "EPSG:25833" );
+        return requestUrlBuilder.toString();
+    }
 
 }
